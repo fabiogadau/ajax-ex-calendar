557cbfd07653050c94d603111c6d1c71f1be3dd5
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-29 15:28:15 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,38 @@
+/*
+* CALENDAR
+Creare un calendario dinamico con le festività. 
+Partiamo dal gennaio 2018 dando la possibilità di cambiare mese, gestendo il caso in cui l’API non possa ritornare festività. 
+Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
+Ogni volta che cambio mese dovrò:
+Controllare se il mese è valido (per ovviare al problema che l’API non carichi holiday non del 2018).
+Controllare quanti giorni ha il mese scelto formando così una lista.
+Chiedere all’api quali sono le festività per il mese scelto.
+Evidenziare le festività nella lista.
+*/
+
+$(document).ready(function() {
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
